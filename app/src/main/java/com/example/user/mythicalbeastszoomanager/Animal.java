b383d8557b39d572239b924bddbe62da1e298f35
Logan Smith <hamspamandeggs@gmail.com>
2016-12-16 10:52:23 +0000

    First commit Have made Animal class with name and age so far going to write tests next

---
+++ app/src/main/java/com/example/user/mythicalbeastszoomanager/Animal.java
@@ -0,0 +1,26 @@
+package com.example.user.mythicalbeastszoomanager;
+
+/**
+ * Created by user on 16/12/2016.
+ */
+public class Animal  {
+
+    String name;
+    int age;
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+
+
+}
